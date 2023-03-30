056ad54c3d5d808074b2f680958ca2196bf79e59
Stefanescu Florian-Alexandru <alexstefanescu128@yahoo.com>
2023-03-30 22:23:30 +0300

    ++

--- src/components/path/path.jsx
+++ src/components/path/path.jsx
@@ -1,22 +1,38 @@
 import './path.css';
 import taroticon from "../../assets/imgs/tarotReadingIcon.png"
-
+import starsBg from '../../assets/starsBg.mp4'
 
 const path = () => {
   return (
     <section id="path">
         <div className="path-container">
-            <h1>CHOOSE YOUR PATH</h1>
-            <div className="tarotreading-container">
-                <img src={taroticon} alt="tarot-icon" />
-                
+
+            <div className="overlay-path"></div>
+
+            <video src={starsBg} autoPlay loop muted className="starsBg"/>
+
+            <div className="content-path">
+              <div className="content-path-title">
+                <h1>CHOOSE YOUR PATH</h1>
+              </div>
+
+              <div className="choose-path">
+                <div className="tarotreading-container">
+                    <img src={taroticon} alt="tarot-icon" />
+                    
+                </div>
+                <div className="horoscope-container">
+                    
+                </div>
+                <div className="crystals-container">
+                    
+                </div>
+              </div>
+              
+              
             </div>
-            <div className="horoscope-container">
-                
-            </div>
-            <div className="crystals-container">
-                
-            </div>
+
+            
         </div>
     </section>
   );
