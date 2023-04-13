0e2f6a4c460c442aa3afae2673c3b3cc3d5d06ce
Stefanescu Florian-Alexandru <alexstefanescu128@yahoo.com>
2023-04-14 00:50:26 +0300

    ++different tittle style

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,8 +1,10 @@
 import './hero.css';
 import videoTarot from '../../assets/videoTarot.mp4'
+import trionfiGIF from '../../assets/trionfiGIF.mp4'
 import React from 'react'
 import downarrow from "../../assets/imgs/downarrow.png"
 import downarrow2 from "../../assets/imgs/downarrow2.png"
+
 
 const hero = () => {
   return (
@@ -11,10 +13,11 @@ const hero = () => {
             <div className="overlay">
 
             </div>
-            <video src={videoTarot} autoPlay loop muted/>
+            <video id='videoTarot' src={videoTarot} autoPlay loop muted/>
             <div className="content">
               <h1>welcome to</h1>
-              <p>Trionfi</p>
+              {/* <p>Trionfi</p> */}
+              <video src={trionfiGIF} autoPlay loop muted/>
               <a href="#path"><img src={downarrow} alt="downarrow" /></a>
             </div>
         </div>
